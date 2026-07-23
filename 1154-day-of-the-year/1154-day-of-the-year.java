5f3a427734ae41aecf507bc7df4a46e20b03d445
Piyushkumar-Thakur <t5743325@gmail.com>
2026-07-23 21:19:09 +0530

    Time: 10 ms (40.48%), Space: 47.3 MB (7.24%) - LeetHub

---
+++ 1154-day-of-the-year/1154-day-of-the-year.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int dayOfYear(String date) {
+         String[] arr = date.split("-");
+
+        int year = Integer.parseInt(arr[0]);
+        int month = Integer.parseInt(arr[1]);
+        int day = Integer.parseInt(arr[2]);
+
+        int[] days = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            days[1] = 29;
+        }
+        int total = day;
+
+        for (int i = 0; i < month - 1; i++) {
+            total += days[i];
+        }
+        return total;
+    }
+}
